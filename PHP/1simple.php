7b0ca64fea13685ebfe0c027ddd896d9e434505b
FaizanAhmed009 <64935853+FaizanAhmed009@users.noreply.github.com>
2024-07-18 10:36:32 +0500

    Add files via upload

---
+++ PHP/1simple.php
@@ -0,0 +1,20 @@
+<?php
+echo "welcome";
+echo "<h1>Welcome to my web page</h1>";
+echo '<table border="1" bordercolor="green" width="300" height="120"><tr>
+<td>abc</td>
+</tr>
+</table>';
+?>
+
+
+
+
+<html>
+<body bgcolor="red">
+<h1>Hello World
+</h1>
+</body>
+
+
+</html>
---
+++ PHP/variable.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title> hello world</title>
+</head>
+<?php
+$y=20;
+echo $y;
+
+$name="<h1>Faizan</h1>";
+echo $name;
+?>
+<body> 
+</body>
+
+
+</html>
